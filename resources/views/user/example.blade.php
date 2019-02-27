671c7c12a7e437741527fdd0dc99eedc1173dfc3
jerichen <jerichen@cw.com.tw>
2019-02-28 00:29:07 +0800

    for test

--- resources/views/user/example.blade.php
+++ resources/views/user/example.blade.php
@@ -1,7 +1,6 @@
-
 <!doctype html>
 <html lang="en">
-  <head>
+<head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta name="description" content="">
@@ -12,62 +11,73 @@
     <link rel="canonical" href="https://getbootstrap.com/docs/4.3/examples/checkout/">
 
     <!-- Bootstrap core CSS -->
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+          integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 
 
     <style>
-      .bd-placeholder-img {
-        font-size: 1.125rem;
-        text-anchor: middle;
-        -webkit-user-select: none;
-        -moz-user-select: none;
-        -ms-user-select: none;
-        user-select: none;
-      }
-
-      @media (min-width: 768px) {
-        .bd-placeholder-img-lg {
-          font-size: 3.5rem;
+        .bd-placeholder-img {
+            font-size: 1.125rem;
+            text-anchor: middle;
+            -webkit-user-select: none;
+            -moz-user-select: none;
+            -ms-user-select: none;
+            user-select: none;
         }
-      }
+
+        @media (min-width: 768px) {
+            .bd-placeholder-img-lg {
+                font-size: 3.5rem;
+            }
+        }
     </style>
     <!-- Custom styles for this template -->
     <link href="form-validation.css" rel="stylesheet">
-  </head>
-  <body class="bg-light">
-    <div class="container">
-        <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
-        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+</head>
+<body class="bg-light">
+<div class="container">
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
+            integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo"
+            crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"
+            integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1"
+            crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"
+            integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM"
+            crossorigin="anonymous"></script>
 
-        <div class="mb-3">
-            <label for="username">Username</label>
-            <div class="input-group">
-              <div class="input-group-prepend">
+    <div class="mb-3">
+        <label for="username">Username</label>
+        <div class="input-group">
+            <div class="input-group-prepend">
                 <span class="input-group-text">@</span>
-              </div>
-              <input type="text" class="form-control" id="username" placeholder="請填入姓名" value="{{$user->name}}">
-              <div class="invalid-feedback" style="width: 100%;">
-                Your username is required.
-              </div>
             </div>
-          </div>
+            <input type="text" class="form-control" id="username" placeholder="請填入姓名" value="{{$user->name}}">
+            <div class="invalid-feedback" style="width: 100%;">
+                Your username is required.
+            </div>
+        </div>
+    </div>
 
-        <div class="mb-3">
-          <label for="email">Email <span class="text-muted">(Optional)</span></label>
-          <input type="email" class="form-control" id="email" placeholder="請填入Email" value="{{$user->email}}">
-          <div class="invalid-feedback">
+    <div class="mb-3">
+        <label for="email">Email <span class="text-muted">(Optional)</span></label>
+        <input type="email" class="form-control" id="email" placeholder="請填入Email" value="{{$user->email}}">
+        <div class="invalid-feedback">
             Please enter a valid email address for shipping updates.
-          </div>
         </div>
+    </div>
 
-        <div class="mb-3">
-          <label for="address">Address</label>
-          <input type="value" class="form-control" id="address" placeholder="請填入地址" value="{{$user->userInfo->address}}">
-          <div class="invalid-feedback">
+    <div class="mb-3">
+        <label for="address">Address</label>
+        <input type="value" class="form-control" id="address" placeholder="請填入地址" value="{{$user->userInfo->address}}">
+
+        {{--給你參考，可以填入假資料看看--}}
+        <input type="value" class="form-control" id="address" placeholder="請填入地址" value="這是假資料地址">
+
+        <div class="invalid-feedback">
             Please enter your shipping address.
-          </div>
         </div>
+    </div>
 
-      </body>
+</body>
 </html>
